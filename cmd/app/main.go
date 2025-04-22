ca28ff98cf8b894d8aa8f2f8a4485532edfff1af
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2025-04-22 15:49:18 -0300

    feat(app): enhance progress tracking and error handling
    Refactor the main function to improve progress tracking and error
    handling. Introduce a new channel for counting lines processed and
    remove the error channel. Update the progress bar with a more
    descriptive style and add file-specific execution for better
    granularity. This change aims to provide clearer feedback during
    the import process and ensure more robust error management.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 	"sync"
 	"time"
 
 	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 
 	immu "github.com/diegodario88/importador-cep-correios/pkg/constants"
 	"github.com/diegodario88/importador-cep-correios/pkg/db"
@@ -20,16 +20,12 @@ import (
 func main() {
 	start := time.Now()
 	var wg sync.WaitGroup
+	var lineCount int64
 	basePath := filepath.Join(utils.GetCWD(), "eDNE", "basico")
 	database := &db.DB{}
 	ctx := context.Background()
-	errCh := make(chan error, immu.SIXTEEN_TASKS)
-	progress := mpb.New(
-		mpb.WithWidth(64),
-		mpb.WithWaitGroup(&wg),
-		mpb.WithOutput(os.Stderr),
-		mpb.WithAutoRefresh(),
-	)
+	counterChan := make(chan work.Counter)
+	progress := mpb.New(mpb.WithWidth(64))
 
 	if err := database.Connect(); err != nil {
 		log.Fatal(err)
@@ -40,46 +36,61 @@ func main() {
 		log.Fatal(err)
 	}
 
-	run := func(execute work.Processes) {
+	bar := progress.New(int64(immu.SIXTEEN_TASKS),
+		mpb.BarStyle().Lbound("╢").Filler("▌").Tip("▌").Padding("░").Rbound("╟"),
+		mpb.BarFillerOnComplete(""),
+		mpb.PrependDecorators(
+			decor.Name("Importação base eDNE Correios:"),
+			decor.OnComplete(
+				decor.Spinner(nil, decor.WCSyncSpace), "done",
+			),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+
+	run := func(fileName string, execute work.Processes) {
 		defer wg.Done()
+		defer bar.Increment()
 
 		tools := work.JobTools{
-			Ctx:      ctx,
-			Database: database,
-			BasePath: basePath,
-			Progress: progress,
+			Ctx:         ctx,
+			Database:    database,
+			BasePath:    basePath,
+			CounterChan: counterChan,
 		}
 
-		if err := execute(tools); err != nil {
-			errCh <- err
-		}
-
+		execute(fileName, tools)
 	}
 
 	wg.Add(immu.SIXTEEN_TASKS)
+	go run("ECT_PAIS.TXT", work.Single)
+	go run("LOG_FAIXA_UF.TXT", work.Single)
+	go run("LOG_LOCALIDADE.TXT", work.Single)
+	go run("LOG_VAR_LOC.TXT", work.Single)
+	go run("LOG_FAIXA_LOCALIDADE.TXT", work.Single)
+	go run("LOG_BAIRRO.TXT", work.Single)
+	go run("LOG_VAR_BAI.TXT", work.Single)
+	go run("LOG_FAIXA_BAIRRO.TXT", work.Single)
+	go run("LOG_CPC.TXT", work.Single)
+	go run("LOG_FAIXA_CPC.TXT", work.Single)
+	go run("LOG_LOGRADOURO_*.TXT", work.Multiple)
+	go run("LOG_VAR_LOG.TXT", work.Single)
+	go run("LOG_NUM_SEC.TXT", work.Single)
+	go run("LOG_GRANDE_USUARIO.TXT", work.Single)
+	go run("LOG_UNID_OPER.TXT", work.Single)
+	go run("LOG_FAIXA_UOP.TXT", work.Single)
 
-	go run(work.ProcessECTPais)
-	go run(work.ProcessLogFaixaUF)
-	go run(work.ProcessLogLocalidade)
-	go run(work.ProcessLogVarLoc)
-	go run(work.ProcessLogFaixaLocalidade)
-	go run(work.ProcessLogBairro)
-	go run(work.ProcessLogVarBai)
-	go run(work.ProcessLogFaixaBairro)
-	go run(work.ProcessLogCPC)
-	go run(work.ProcessLogFaixaCPC)
-	go run(work.ProcessLogLogradouro)
-	go run(work.ProcessLogVarLog)
-	go run(work.ProcessLogNumSec)
-	go run(work.ProcessLogGrandeUsuario)
-	go run(work.ProcessLogUnidOper)
-	go run(work.ProcessLogFaixaUOP)
+	go func() {
+		wg.Wait()
+		close(counterChan)
+	}()
 
-	wg.Wait()
-	close(errCh)
+	for result := range counterChan {
+		if result.Error != nil {
+			log.Fatalf("Erro no processamento: %v", result.Error)
+		}
 
-	for err := range errCh {
-		log.Fatalf("Erro no processamento: %v", err)
+		lineCount += int64(result.Increment)
 	}
 
 	progress.Wait()
@@ -90,5 +101,7 @@ func main() {
 
 	fmt.Printf("Registros totais: %s\n", utils.FormatNumber(totalRecords))
 	fmt.Printf("Total de CEPs: %s\n", utils.FormatNumber(totalCeps))
+	fmt.Printf("Total de linhas: %s\n", utils.FormatNumber(int(lineCount)))
 	fmt.Printf("Tempo total: %s\n", time.Since(start).Round(time.Millisecond))
+	log.Println("------------------------------------------------------")
 }
